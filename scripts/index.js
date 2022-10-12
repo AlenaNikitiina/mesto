6dc8e36547c990e6724625f6a8ddfd0aa3981af6
Alena Nikitina <alena.tkachenk@mail.ru>
2022-10-12 21:09:47 +0300

    fix popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,11 @@
 // Находим форму в DOM
 const formElement = document.querySelector('.popup__form');
+const form = document.querySelector('.form')
 // Про открытие и закрытие попапа
 const popupEdit = document.querySelector('.popup_edit');// нашли попапы
 const popupAdd = document.querySelector('.popup_add');
 const popupZoom = document.querySelector('.popup_zoom');
+const popupAll = document.querySelector('.popup')
 // Находим поля формы в DOM, в которых можно изменения писать
 const nameInput = document.querySelector('.nameInput');
 const jobInput = document.querySelector('.jobInput');
@@ -64,8 +66,23 @@ buttonOpenAdd.addEventListener('click', () => {
   openPopup(popupAdd);
 });
 
+// функция закрытия попапов по нажатию на Escape
+const closeByEscape = (evt) => {
+  if (evt.key === 'Escape') {
+    const openedNowPopup = document.querySelector('.popup_opened')
+    closePopup(openedNowPopup);
+  }
+}
 
-// Основаная функция. которая создает карточку с линками и именами из массива выше
+// функция закрыть попап нажав на оверлей
+const closeByOverlay =  (evt) => {
+  if (evt.target.classList.contains('popup_opened')) {
+    closePopup(evt.target);
+  }
+}
+document.addEventListener('click', closeByOverlay);
+
+// Основная функция. которая создает карточку с линками и именами из массива выше
 function createCard(name, link) {
   const newHtmlElement = templateItem.cloneNode(true); //копируем содержимое тега темплате
   //наполняем содержимым
@@ -129,95 +146,9 @@ function addCard(name, link) {
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 popupEdit.addEventListener('submit', submitHandlerForm);
 popupAdd.addEventListener('submit', createNewCard);
+//form.addEventListener('submit', submitHandlerForm);
+//form.addEventListener('submit', createNewCard);
+
+//popupAll.addEventListener('click', closeByOverlay);
 
 
-//// Валидация
-// 1 Функция, которая добавляет класс с ошибкой
-const showInputError = (formElement, inputElement, errorMessage, setting) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // Находим элемент ошибки внутри самой функции
-  inputElement.classList.add(setting.inputErrorClass);  // добавьте класс ошибки элементу input
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(setting.inputErrorClassActiv); // Показываем сообщение об ошибке
-}
-
-// 2 Функция, которая удаляет класс с ошибкой
-const hideInputError = (formElement, inputElement, setting) => {
-  // Находим элемент ошибки
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(setting.inputErrorClass);
-  errorElement.classList.remove(setting.inputErrorClassActiv); // Скрываем сообщение об ошибке
-  errorElement.textContent = ''; // Очистим ошибку
-}
-
-// 3 Функция, которая проверяет валидность поля
-const isValid = (formElement, inputElement, setting) => {
-  if (!inputElement.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formElement, inputElement, inputElement.validationMessage, setting); // получает параметром форму, в которой находится проверяемое поле, и само это поле
-  } else {
-    // Если проходит, скроем
-    hideInputError(formElement, inputElement, setting);
-  }
-}
-
-// 7 Функция кот вкл откл кнопку. принимает массив полей ввода и элемент кнопки, состояние которой нужно менять
-const toggleButtonState = (inputList, buttonElement, setting) => {
-  // Если есть хотя бы один невалидный инпут
-  if (hasInvalidInput(inputList, setting)) {
-    buttonElement.classList.add(setting.buttonElementInactiv); // сделай кнопку неактивной
-  } else {
-    buttonElement.classList.remove(setting.buttonElementInactiv); // сделай кнопку активной
-  }
-}
-
-// 4 Пусть слушатель событий добавится всем полям ввода внутри формы.
-const setEventListeners = (formElement, setting) => {
-  const inputList = Array.from(formElement.querySelectorAll(setting.inputSelector));  // Находим все поля внутри формы, сделаем из них массив
-  const buttonElement = formElement.querySelector(setting.submitButtonSelector);
-  toggleButtonState(inputList, buttonElement, setting);  // Вызовем toggleButtonState, чтобы не ждать ввода данных в поля
-
-  // Обойдём все элементы полученной коллекции
-  inputList.forEach((inputElement) => {
-    // каждому полю добавим обработчик события input
-    inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement, setting); // Внутри колбэка вызовем isValid,передав ей форму и проверяемый элемент
-      toggleButtonState(inputList, buttonElement, setting); // Вызовем toggleButtonState и передадим ей массив полей и кнопку
-    });
-  });
-};
-
-// 5 функция которая найдёт и переберёт все формы на странице
-const enableValidation = (setting) => {
-  // Найдём все формы с указанным классом в DOM, сделаем из них массив
-  const formList = Array.from(document.querySelectorAll(setting.formSelector));
-  // Переберём его
-  formList.forEach((formElement) => {
-    setEventListeners(formElement, setting); // Для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
-  });
-};
-
-// 6 функция которая проверит все инпуты в форме валидное/нет, принимает массив полей
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid; // Если поле не валидно, колбэк вернёт true Обход массива прекратится и вся функция вернёт true
-  })
-};
-
-enableValidation (setting);
-
-// 8 функция закрытия попапов по нажатию на Escape
-const closeByEscape = (evt) => {
-  if (evt.key === 'Escape') {
-    const openedNowPopup = document.querySelector('.popup_opened')
-    closePopup(openedNowPopup);
-  }
-}
-
-// 9 функция закрыть попап нажав на оверлей
-const closeByOverlay =  (evt) => {
-  if (evt.target.classList.contains('popup_opened')) {
-    closePopup(evt.target);
-  }
-}
-
-document.addEventListener('click', closeByOverlay);
--- scripts/validate.js
+++ scripts/validate.js
@@ -6,4 +6,78 @@ const setting = {
   submitButtonSelector: '.form__submit',
   buttonElementInactiv: 'form__submit_inactive',
 }
+formElement
 
+//// Валидация
+// 1 Функция, которая добавляет класс с ошибкой
+const showInputError = (formElement, inputElement, errorMessage, setting) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // Находим элемент ошибки внутри самой функции
+  inputElement.classList.add(setting.inputErrorClass);  // добавьте класс ошибки элементу input
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(setting.inputErrorClassActiv); // Показываем сообщение об ошибке
+}
+
+// 2 Функция, которая удаляет класс с ошибкой
+const hideInputError = (formElement, inputElement, setting) => {
+  // Находим элемент ошибки
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(setting.inputErrorClass);
+  errorElement.classList.remove(setting.inputErrorClassActiv); // Скрываем сообщение об ошибке
+  errorElement.textContent = ''; // Очистим ошибку
+}
+
+// 3 Функция, которая проверяет валидность поля
+const isValid = (formElement, inputElement, setting) => {
+  if (!inputElement.validity.valid) {
+    // Если поле не проходит валидацию, покажем ошибку
+    showInputError(formElement, inputElement, inputElement.validationMessage, setting); // получает параметром форму, в которой находится проверяемое поле, и само это поле
+  } else {
+    // Если проходит, скроем
+    hideInputError(formElement, inputElement, setting);
+  }
+}
+
+// 7 Функция кот вкл/откл кнопку. принимает массив полей ввода и элемент кнопки, состояние которой нужно менять
+const toggleButtonState = (inputList, buttonElement, setting) => {
+  // Если есть хотя бы один невалидный инпут
+  if (hasInvalidInput(inputList, setting)) {
+    buttonElement.classList.add(setting.buttonElementInactiv); // сделай кнопку неактивной
+  } else {
+    buttonElement.classList.remove(setting.buttonElementInactiv); // сделай кнопку активной
+  }
+}
+
+// 4 функция кот добавляет слушатель событий добавится всем полям ввода внутри формы.
+const setEventListeners = (formElement, setting) => {
+  const inputList = Array.from(formElement.querySelectorAll(setting.inputSelector));  // Находим все поля внутри формы, сделаем из них массив
+  const buttonElement = formElement.querySelector(setting.submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, setting);  // Вызовем toggleButtonState, чтобы не ждать ввода данных в поля
+
+  // Обойдём все элементы полученной коллекции
+  inputList.forEach((inputElement) => {
+    // каждому полю добавим обработчик события input
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement, setting); // Внутри колбэка вызовем isValid,передав ей форму и проверяемый элемент
+      toggleButtonState(inputList, buttonElement, setting); // Вызовем toggleButtonState и передадим ей массив полей и кнопку
+    });
+  });
+};
+
+// 5 функция которая найдёт и переберёт все формы на странице
+const enableValidation = (setting) => {
+  // Найдём все формы с указанным классом в DOM, сделаем из них массив
+  const formList = Array.from(document.querySelectorAll(setting.formSelector));
+  // Переберём его
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, setting); // Для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
+  });
+};
+
+// 6 функция которая проверит все инпуты в форме валидное/нет, принимает массив полей
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid; // Если поле не валидно, колбэк вернёт true Обход массива прекратится и вся функция вернёт true
+  })
+};
+
+enableValidation (setting);
