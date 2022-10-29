b67d85162004b5c0c448f7fc55aff0288013fae2
Alena Nikitina <alena.tkachenk@mail.ru>
2022-10-29 13:27:10 +0300

    createCard вынесли в card.js

--- scripts/index.js
+++ scripts/index.js
@@ -86,44 +86,6 @@ popupAll.forEach((item) => {
 
 ////
 
-// Основная функция. которая создает карточку с линками и именами из массива выше
-function createCard(name, link) {
-  const newHtmlElement = templateItem.cloneNode(true); //копируем содержимое тега темплате
-  //наполняем содержимым
-  newHtmlElement.querySelector('.element__title').textContent = name;
-  const fotoZoomOpen = newHtmlElement.querySelector('.element__foto'); // открыть попап зум картинки
-  fotoZoomOpen.src = link;
-  fotoZoomOpen.alt = name;
-
-  //лайк
-  const buttonLike = newHtmlElement.querySelector('.element__like'); // нашли кнопку лайка
-  buttonLike.addEventListener('click', (item) => {
-    likeIt(buttonLike);
-  });
-
-  //функция постановки лайка
-  function likeIt (item) {
-  item.classList.toggle('element__like_active');
-  };
-
-  //мусорка
-  const trashButton = newHtmlElement.querySelector('.element__trash-button'); // нашли кнопку мусорки
-  trashButton.addEventListener("click", (evt) => {
-    const currentPhoto =  evt.target.closest('.elements__card');
-    currentPhoto.remove();
-  });
-
-  // увелечение фотографий
-  fotoZoomOpen.addEventListener('click', function (evt) {
-    const picture = evt.target;
-    popupImage.src = picture.src;
-    popupImage.alt = picture.alt;
-    popupFigcaption.textContent = picture.alt;
-    openPopup(popupZoom);
-  });
-
-  return newHtmlElement;
-}
 
 // функция создания карточек для каждого эл-та из массива. (переберет 6 раз и каждому назначит имя, линк, альт)
 function render () {
